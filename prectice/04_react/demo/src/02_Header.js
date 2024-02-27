a379c10ef551cec8bdb2b2e1e6f02f8faa8c5b2a
thesiyahemali <thesiyahemali999@gmail.com>
2024-02-27 15:41:32 +0530

    update

---
+++ prectice/04_react/demo/src/02_Header.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './05_Card1.css'
+import { Link,NavLink } from 'react-router-dom'
+
+const header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <NavLink activeclassName='active' aria-current="page" to={'home'}>Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+
+        <li className="nav-item">
+          <Link className="nav-link" to={'/concat/profile'}>profile</Link>
+        </li>
+
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><Link className="dropdown-item" to={'/concat'}>Action</Link></li>
+            <li><Link className="dropdown-item" to={'/haderactive'}>Another action</Link></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
+
+export default header
+
